98bd41cd754351f4f310c87bd112d11360e7a09a
Luca Spennato <luca.spennato@gmail.com>
2022-11-18 15:16:28 +0100

    tv and cuffie class set

--- src/generation/italy/shop/Televisori.java
+++ src/generation/italy/shop/Televisori.java
@@ -1,5 +1,33 @@
 package generation.italy.shop;
 
-public class Televisori {
+public class Televisori extends Prodotto{
 
+	int pollici;
+	boolean isSmartTv;
+	
+	public Televisori(String marca, int prezzo, int pollici, boolean isSmartTv) {
+		
+		super(marca, prezzo);
+		this.pollici = pollici;
+		this.isSmartTv = isSmartTv;
+	}
+
+	public int getPollici() {
+		return pollici;
+	}
+
+	public void setPollici(int pollici) {
+		this.pollici = pollici;
+	}
+
+	public boolean isSmartTv() {
+		return isSmartTv;
+	}
+
+	public void setSmartTv(boolean isSmartTv) {
+		this.isSmartTv = isSmartTv;
+	}
+	
+	
+	
 }
