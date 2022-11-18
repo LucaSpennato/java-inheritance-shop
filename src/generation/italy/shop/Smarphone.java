af7d4921aff4ae6d220f06767b9b77993dc10b62
Luca Spennato <luca.spennato@gmail.com>
2022-11-18 15:07:12 +0100

    smartphone set

--- src/generation/italy/shop/Smarphone.java
+++ src/generation/italy/shop/Smarphone.java
@@ -1,5 +1,36 @@
 package generation.italy.shop;
 
+import java.util.Random;
+
 public class Smarphone extends Prodotto{
 
+	String IMEI;
+	int memoria;
+	
+	public Smarphone(String marca, int prezzo, int memoria) {
+		
+		super(marca, prezzo);
+		this.memoria = memoria;
+		setIMEI();
+	}
+
+	public String getIMEI() {
+		return IMEI;
+	}
+
+	private void setIMEI() {
+		Random rnd = new Random();
+		IMEI = rnd.nextInt(999999999) + "" + rnd.nextInt(99999);
+	}
+
+	public int getMemoria() {
+		return memoria;
+	}
+
+	public void setMemoria(int memoria) {
+		this.memoria = memoria;
+	}
+
+	
+	
 }
